69d0ef028b20fe546dd8a67281960f44bea968e3
James Benjamin <james.abenj@gmail.com>
2020-03-07 23:01:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,12 @@ class Application
      req = Rack::Request.new(env)
 
      if req.path=="/items/<ITEM NAME>"
-       item = req.params["item"]
-       if @@items.include?(item)
-       resp.write Item.find_by(name, "#{item}").price
+      item = req.params["item"]
+      if @@items.include?(item)
+      resp.write Item.find_by(name, "#{item}").price
+      else
+        resp.status = 400
+        resp.write "Item not found"
      else
        resp.write "Route not found"
        resp.status = 404
