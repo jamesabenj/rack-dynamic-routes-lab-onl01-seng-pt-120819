8d32da2c6e6204f73fd4f2b52b044d53335993c0
James Benjamin <james.abenj@gmail.com>
2020-03-07 23:06:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
      req = Rack::Request.new(env)
 
      if req.path=="/items/<ITEM NAME>"
-      item = req.params["ITEM NAME"]
+      item = req.params["<ITEM NAME>"]
       @@items.each do |i|
         if i.name = item
         resp.write Item.find_by(name, "#{item}").price
